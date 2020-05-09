abdb1150d30dd7476b93e18c7680d02ef162b4ae
daminisalwe1234 <salwedamini2gmail.com>
2020-05-09 13:19:22 +0530

    coin fllip

---
+++ FlipCoinStimulator.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -x
+heads=0
+tails=0
+
+flipCoin() {
+	result=$((RANDOM % 2))
+	if [ $result -eq 0 ]
+	then
+		$((tails++))
+	else
+		$((heads++))
+	fi
+}
+
+while [ $tails -le 21 -o $heads -le 21 ]
+do
+	flipCoin
+done
+
+if [ $heads -eq $tails ]
+then
+	while [ $(($heads -$tails)) -ne 2 -o $(($tails - $heads)) -ne 2 ]
+	do
+		flipCoin
+	done
+elif [ $heads -gt $tails ]
+then
+	echo "Heads Wins"
+elif [ $heads -lt $tails ]
+then
+	echo"Tails Wins"
+fi
+
