1ab577a5da4886485ca94a2063ced292949b8aa3
daminisalwe1234 <salwedamini2gmail.com>
2020-05-09 14:04:34 +0530

    coin fllip branch2

---
+++ branch2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+read -p "Enter times to flip: " num
+heads=0
+tails=0
+count=0
+while [ $count -ne $num ]
+do
+	$((count++))
+	result=$((RANDOM % 2))
+	if [ $result -eq 0 ]
+	then
+		$((tails++))
+	else
+		$((heads++))
+	fi
+done
+echo "Heads: " $heads
+echo "Tails: " $tails
